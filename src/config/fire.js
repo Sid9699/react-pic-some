19686adc872b38a0328b03fe146c842f320c1753
siddharth <ranasiddharth6@gmail.com>
2022-12-17 15:50:05 +0530

    firebase config update

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC2eIbxLu5KC2zL_7JFem33PXnt3FRbO5o",
-    authDomain: "pic-some.firebaseapp.com",
-    databaseURL: "https://pic-some.firebaseio.com",
-    projectId: "pic-some",
-    storageBucket: "pic-some.appspot.com",
-    messagingSenderId: "557719856117",
-    appId: "1:557719856117:web:aa9b88f6e8eafde0e076bc"
-  };
+  apiKey: "AIzaSyC3qlLBLUlxr12MZ1EZg1OD-u5Yv3LyO0g",
+  authDomain: "picsome-a7a9f.firebaseapp.com",
+  projectId: "picsome-a7a9f",
+  storageBucket: "picsome-a7a9f.appspot.com",
+  messagingSenderId: "527481958059",
+  appId: "1:527481958059:web:4f1d97c6af8de5f6e711ee",
+  measurementId: "G-8F8TK45KTF",
+};
 
-  const fire = firebase.initializeApp(firebaseConfig)
+const fire = firebase.initializeApp(firebaseConfig);
 
-  export default fire
+export default fire;
